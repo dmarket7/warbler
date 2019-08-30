a1c30b741a2038d216caade9258b04a60e23e672
stephenau93 <stephenau93@gmail.com>
2019-08-30 16:01:19 -0700

    test

--- app.py
+++ app.py
@@ -34,13 +34,15 @@ login_manager.init_app(app)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('postgres://ozibortidvmjrz:11d564c5697d2a82c86fbe8f0c479ac0ba5e3ffa013cd4bef7eca7c4d8f555f9@ec2-54-83-33-14.compute-1.amazonaws.com:5432/dedtj3givh6msh', 'postgres:///warbler'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+    # os.environ.get('postgres://ozibortidvmjrz:11d564c5697d2a82c86fbe8f0c479ac0ba5e3ffa013cd4bef7eca7c4d8f555f9@ec2-54-83-33-14.compute-1.amazonaws.com:5432/dedtj3givh6msh', 'postgres:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgres:///warbler')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "nevertell")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
