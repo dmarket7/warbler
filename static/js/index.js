2775d0c6bcac35c681048b82adcba1e5ea692402
stephenau93 <stephenau93@gmail.com>
2019-12-11 11:55:14 -0800

    Fix likes

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+/** AJAX Requests for Liking/Disliking posts **/ 
+
+let $body = $("body");
+let $like = $("#like-button");
+
+$body.on('click', $like, async function(e) {
+  e.preventDefault();
+  
+})
